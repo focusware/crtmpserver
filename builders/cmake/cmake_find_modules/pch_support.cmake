7a010c906cd86315bcabe043739c161c7aff86d8
Boris Nagels <develop@focusware.nl>
2019-10-15 10:20:22 +0200

    Fixed: disable the generation of the precompiled header for now.
    If you use a newer version of ninja, a error will be generated and the compile process stops:
    ninja: error: build.ninja:2073: multiple rules generate precompiled.gch/Release.c++/common.gch [-w dupbuild=err]

--- builders/cmake/cmake_find_modules/pch_support.cmake
+++ builders/cmake/cmake_find_modules/pch_support.cmake
@@ -22,7 +22,7 @@ ENDIF()
 
 MACRO(ADD_PRECOMPILED_HEADER _targetName _input )
 	#get the file name (no path)
-	GET_FILENAME_COMPONENT(_name ${_input} NAME_WE)
+#	GET_FILENAME_COMPONENT(_name ${_input} NAME_WE)
 
 #	GET_DIRECTORY_PROPERTY(__allvars VARIABLES)
 #	#MESSAGE(STATUS "VARIABLES on ${_targetName}: ${__allvars}")
@@ -36,53 +36,53 @@ MACRO(ADD_PRECOMPILED_HEADER _targetName _input )
 #	MESSAGE(FATAL_ERROR "asdasdsa")
 
 	#locate the file
-	SET(_source "${_input}")
+#	SET(_source "${_input}")
 
 	#compute the output directory
-	SET(_outdir "${CMAKE_BINARY_DIR}/precompiled.gch/${CMAKE_BUILD_TYPE}.c++")
+#	SET(_outdir "${CMAKE_BINARY_DIR}/precompiled.gch/${CMAKE_BUILD_TYPE}.c++")
 
 	#create the output directory
-	MAKE_DIRECTORY(${_outdir})
+#	MAKE_DIRECTORY(${_outdir})
 
 	#compute the output file
-	SET(_output "${_outdir}/${_name}.gch")
+#	SET(_output "${_outdir}/${_name}.gch")
 
 	#get the compiler flags
-	STRING(TOUPPER "CMAKE_CXX_FLAGS_${CMAKE_BUILD_TYPE}" _flags_var_name)
-	SET(_compiler_FLAGS ${CMAKE_CXX_FLAGS} ${${_flags_var_name}})
+#	STRING(TOUPPER "CMAKE_CXX_FLAGS_${CMAKE_BUILD_TYPE}" _flags_var_name)
+#	SET(_compiler_FLAGS ${CMAKE_CXX_FLAGS} ${${_flags_var_name}})
 
 	#get the list of include directories for the current project and compute the new version of compiler flags by
 	#appinding -I
-	GET_DIRECTORY_PROPERTY(_directory_flags INCLUDE_DIRECTORIES)
-	FOREACH(item ${_directory_flags})
-		LIST(APPEND _compiler_FLAGS "-I${item}")
-	ENDFOREACH()
+#	GET_DIRECTORY_PROPERTY(_directory_flags INCLUDE_DIRECTORIES)
+#	FOREACH(item ${_directory_flags})
+#		LIST(APPEND _compiler_FLAGS "-I${item}")
+#	ENDFOREACH()
 
 	#get the definitions of the current project and append them as well to the current compiler flags
-	GET_DIRECTORY_PROPERTY(_directory_flags COMPILE_DEFINITIONS)
-	FOREACH(item ${_directory_flags})
-		LIST(APPEND _compiler_FLAGS "-D${item}")
-	ENDFOREACH()
+#	GET_DIRECTORY_PROPERTY(_directory_flags COMPILE_DEFINITIONS)
+#	FOREACH(item ${_directory_flags})
+#		LIST(APPEND _compiler_FLAGS "-D${item}")
+#	ENDFOREACH()
 
 	#now split the entire thing
-	SEPARATE_ARGUMENTS(_compiler_FLAGS)
+#	SEPARATE_ARGUMENTS(_compiler_FLAGS)
 
 	#compute the new command
-	ADD_CUSTOM_COMMAND(
-		OUTPUT ${_output}
-		COMMAND ${CMAKE_CXX_COMPILER} ${PCH_CXX_FLAGS} ${_compiler_FLAGS} -x c++-header -c -o ${_output} ${_source}
-		DEPENDS ${_source}
-	)
+#	ADD_CUSTOM_COMMAND(
+#		OUTPUT ${_output}
+#		COMMAND ${CMAKE_CXX_COMPILER} ${PCH_CXX_FLAGS} ${_compiler_FLAGS} -x c++-header -c -o ${_output} ${_source}
+#		DEPENDS ${_source}
+#	)
 
 	#add a new target to the project
-	ADD_CUSTOM_TARGET(${_targetName}_${_name}_gch DEPENDS ${_output})
-	ADD_DEPENDENCIES(${_targetName} ${_targetName}_${_name}_gch)
+#	ADD_CUSTOM_TARGET(${_targetName}_${_name}_gch DEPENDS ${_output})
+#	ADD_DEPENDENCIES(${_targetName} ${_targetName}_${_name}_gch)
 
-	EXECUTE_PROCESS(COMMAND ${CMAKE_CXX_COMPILER} --version OUTPUT_VARIABLE _COMPILER_VERSION)
-	STRING(TOLOWER "${_COMPILER_VERSION}" _COMPILER_VERSION)
-	IF(_COMPILER_VERSION MATCHES ".*(clang|llvm).*")
-		SET_TARGET_PROPERTIES(${_targetName} PROPERTIES COMPILE_FLAGS "-include-pch ${_output} -Winvalid-pch")
-	ELSE()
-		SET_TARGET_PROPERTIES(${_targetName} PROPERTIES COMPILE_FLAGS "-I${_outdir} -include ${_name} -Winvalid-pch")
-	ENDIF()
+#	EXECUTE_PROCESS(COMMAND ${CMAKE_CXX_COMPILER} --version OUTPUT_VARIABLE _COMPILER_VERSION)
+#	STRING(TOLOWER "${_COMPILER_VERSION}" _COMPILER_VERSION)
+#	IF(_COMPILER_VERSION MATCHES ".*(clang|llvm).*")
+#		SET_TARGET_PROPERTIES(${_targetName} PROPERTIES COMPILE_FLAGS "-include-pch ${_output} -Winvalid-pch")
+#	ELSE()
+#		SET_TARGET_PROPERTIES(${_targetName} PROPERTIES COMPILE_FLAGS "-I${_outdir} -include ${_name} -Winvalid-pch")
+#	ENDIF()
 ENDMACRO()
